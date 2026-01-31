b43aa7cf4f7c45adad1135662dc413d607ee4e35
Simone Siragusa <sira.simon@icloud.com>
2026-01-31 13:58:26 +0100

    Tutorial finito

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,10 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
 export default defineConfig({
-    site: "https://srgsmn-astoblogtutorial.netlify.app"
+  site: "https://srgsmn-astoblogtutorial.netlify.app",
+  integrations: [preact()]
 });
